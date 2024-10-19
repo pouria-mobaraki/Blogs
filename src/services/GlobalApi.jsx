c87b7efd7213c2510013245c015621793ffda9ba
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-10-19 16:42:24 +0330

    change url

--- src/services/GlobalApi.jsx
+++ src/services/GlobalApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Base_URL = 'http://localhost:3000/api/introPost.json'
+const Base_URL = '/api/introPost.json'
 
 const getPost = axios.get(Base_URL)
 const getPostById = (id) => axios.get(Base_URL)
